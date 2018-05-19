e6243581d94d80643146ddd654c510e28209fa32
Dani <martin.villalobos@softtek.com>
2018-05-20 00:16:05 +0200

    Updated tagname for taskdefinition and deploy script

--- deploy.sh
+++ deploy.sh
@@ -18,11 +18,11 @@ REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NA
 
 echo "-- Replace the build number and respository URI placeholders with the constants above"
 #Replace the build number and respository URI placeholders with the constants above
-sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-${BUILD_NUMBER}.json
 
 echo "-- Register the task definition in the repository"
 #Register the task definition in the repository
-aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
+aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-${BUILD_NUMBER}.json --region ${REGION}
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 
 echo "-- Get latest revision"
